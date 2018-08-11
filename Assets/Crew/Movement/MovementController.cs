bb9882a76af98b934a7ddf7c73a933e87c1c0be9
Braden Jageman <bjageman@surgeforward.com>
2018-08-10 23:49:21 -0700

    Cleaned up a bunch of code

--- Assets/Crew/Movement/MovementController.cs
+++ Assets/Crew/Movement/MovementController.cs
@@ -6,6 +6,12 @@ public class MovementController : MonoBehaviour {
 	public Waypoint currentWaypoint;
 	public float waypointDistanceThreshold = .1f;
 
+	PathManager pathManager;
+
+	void Start(){
+		pathManager = FindObjectOfType<PathManager>();
+	}
+
 	void MoveTowardsWaypoint(Waypoint waypoint){
 		//StartCoroutine(Patrol());
 	}
--- Assets/Crew/Movement/Waypoint.cs
+++ Assets/Crew/Movement/Waypoint.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class RenameWaypoint : MonoBehaviour {
+public class Waypoint : MonoBehaviour {
 	[SerializeField] string roomName;
 	[SerializeField] float gizmosWaypointSize = .1f;
-	public RenameWaypoint connectedWaypoint;
+	public Waypoint connectedWaypoint;
+	
+	public List<Waypoint> neighbors;
+	public Waypoint previous { get; set; }
+	public float distance { get; set; }
 
 	
 	
