dc1ce2f937ae09579d1ca7cca6b2dde34d50c009
Jacob Lerche <jacob.lerche@gmail.com>
2018-08-11 05:40:09 -0700

    adds a* pathfinding, renames waypoint and waypointsystem to avoid namespace clash

---
+++ Assets/RenameWP.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RenameWaypoint : MonoBehaviour {
+	[SerializeField] string roomName;
+	[SerializeField] float gizmosWaypointSize = .1f;
+	public RenameWaypoint connectedWaypoint;
+
+	
+	
+}
