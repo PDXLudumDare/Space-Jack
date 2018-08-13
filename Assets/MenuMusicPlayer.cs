6c4263a3c7d780666442928b115a51af1ab0c7d3
Braden Jageman <bjageman@surgeforward.com>
2018-08-12 23:00:55 -0700

    added music menu

---
+++ Assets/MenuMusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusicPlayer : MonoBehaviour {
+
+	AudioSource audioSource;
+
+	// Use this for initialization
+	void Awake () {
+		if (FindObjectsOfType<MenuMusicPlayer>().Length > 1){
+			Destroy(this);
+		}
+	}
+	
+	void Start(){
+		MusicPlayer levelMusicPlayer = FindObjectOfType<MusicPlayer>();
+		if (levelMusicPlayer){
+			Destroy(levelMusicPlayer);
+		}
+		print("STARTING");
+		audioSource = GetComponent<AudioSource>();
+		audioSource.Play();
+	}
+}
